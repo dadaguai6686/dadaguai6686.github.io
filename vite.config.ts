8cf12cbc2043e6334546389352827a8afb5b3ad7
dadaguai6686 <2737897632@qq.com>
2026-06-06 15:44:46 +0800

    Fix GitHub Pages root deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        chunkFileNames: "assets/[name].js",
+        entryFileNames: "assets/[name].js"
+      }
+    }
+  },
   server: {
     host: "127.0.0.1",
     port: 5173
